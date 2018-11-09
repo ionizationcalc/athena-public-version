2d6d3bb320f3010c62f133717f449943ed8a8fcd
Chengcai Shen <chengcaishen@cfa.harvard.edu>
2018-11-09 16:35:36 -0500

    Update src/pgen/fluxrope.cpp
    Failed at the bottom BC.

--- src/pgen/fluxrope.cpp
+++ src/pgen/fluxrope.cpp
@@ -114,9 +114,9 @@ void MeshBlock::UserWorkInLoop(void) {
   for(int k=ks; k<=ke; k++) {
     for(int j=js; j<=je; j++) {
       for(int i=is; i<=ie; i++) {
-        //phydro->u(IEN,k,j,i) = phydro->u(IEN,k,j,i)
-        //  -0.5*SQR(phydro->u(IM3,k,j,i))/phydro->u(IDN,k,j,i);
-        //phydro->u(IM3,k,j,i) = 0.0;
+        phydro->u(IEN,k,j,i) = phydro->u(IEN,k,j,i)
+          -0.5*SQR(phydro->u(IM3,k,j,i))/phydro->u(IDN,k,j,i);
+        phydro->u(IM3,k,j,i) = 0.0;
       }
     }
   }
@@ -153,7 +153,6 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   // Read input arguments
   Real gm1 = peos->GetGamma() - 1.0;
   
-
   // Initialize the flux rope
   fr_case = pin->GetInteger("problem","fr_case");
   if (fr_case == 1) {
@@ -165,11 +164,11 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
     fr_rmom = 1.0;
   } else if (fr_case == 2) {
     fr_d  = pin->GetReal("problem","fr_d");
-    fr_h = 0.75*fr_d;
+    fr_h = 1.25*fr_d;
     fr_ri = 0.15*fr_d;
     fr_del = 0.0;
-    fr_rja = 300.0;
-    fr_rmom = fr_d * fr_d * 125. / 32. * 0.80;
+    fr_rja = 3000.0;
+    fr_rmom = fr_d*fr_d*125./32.* 0.80;
   } else {
     printf("Error: define fluxrope.");
   }
@@ -203,16 +202,16 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   for (int k=ks; k<=ke; k++) {
   for (int j=js; j<=je; j++) {
   for (int i=is; i<=ie+1; i++) {
-    //pfield->b.x1f(k,j,i) = (az(j+1,i) - az(j,i))/pcoord->dx2f(j);
-    yc = 0.5*(pcoord->x2f(j) + pcoord->x2f(j+1));
-    pfield->b.x1f(k,j,i) = func_bmx(pcoord->x1f(i), yc);
+    pfield->b.x1f(k,j,i) = (az(j+1,i) - az(j,i))/pcoord->dx2f(j);
+    //yc = 0.5*(pcoord->x2f(j) + pcoord->x2f(j+1));
+    //pfield->b.x1f(k,j,i) = func_bmx(pcoord->x1f(i), yc);
   }}}
   for (int k=ks; k<=ke; k++) {
   for (int j=js; j<=je+1; j++) {
   for (int i=is; i<=ie; i++) {
-    //pfield->b.x2f(k,j,i) = (az(j,i) - az(j,i+1))/pcoord->dx1f(i);
-    xc = 0.5*(pcoord->x1f(i) + pcoord->x1f(i+1));
-    pfield->b.x2f(k,j,i) = func_bmy(xc, pcoord->x2f(j));
+    pfield->b.x2f(k,j,i) = (az(j,i) - az(j,i+1))/pcoord->dx1f(i);
+    //xc = 0.5*(pcoord->x1f(i) + pcoord->x1f(i+1));
+    //pfield->b.x2f(k,j,i) = func_bmy(xc, pcoord->x2f(j));
   }}}
   for (int k=ks; k<=ke+1; k++) {
   for (int j=js; j<=je; j++) {
@@ -248,13 +247,13 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
 Real func_pini(Real x, Real y) {
   Real r = sqrt(x*x + (y-fr_h)*(y-fr_h));
   Real p;
-  p = 1.0/gamma_const;
+  p = 10.0/gamma_const;
   return p;
 }
 
 Real func_teini(Real x, Real y) {
   Real te;
-  te = 1.0/gamma_const;
+  te = 0.5/gamma_const;
   return te;
 }
 
@@ -264,23 +263,23 @@ Real func_rhoini(Real x, Real y) {
   p = func_pini(x, y);
   te = func_teini(x, y);
 
-  /* Add a dense (low temperature) fluxrope core
+  /* Add a dense (low temperature) fluxrope core */
   Real pi = 3.14159265358979;
   Real r, r1, r2, r_edge;
   Real t_inner, t_outer;
+  Real bphi2, bphi2_max, ratio;
   t_outer = te;
   t_inner = t_outer*1.0e-2;
-  r1 = fr_ri - 0.5*fr_del;
-  r_edge = fr_del;
+  r2 = fr_ri + 0.5*fr_del;
+  r_edge = 0.1;
   r = sqrt(x*x + pow(y-fr_h,2)); 
-  if (r <= r1) {
+  if (r <= r2) {
     te = t_inner;
-  } else if (r <= r1 + r_edge) {
-    te = t_inner + 0.5*(t_outer-t_inner)*(1.0-cos(pi*(r-r1)/r_edge));
+  } else if (r <= r2 + r_edge) {
+    te = t_inner + 0.5*(t_outer-t_inner)*(1.0-cos(pi*(r-r2)/r_edge));
   } else {
     te = te;
   }
-  */
 
   // Get non-dimensional density
   rho = p/te;
@@ -625,13 +624,14 @@ void LintInnerX2(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim,
     }
   }
   // (b) Set velocity and density
+  Real xc, yc;
   for (int k=ks; k<=ke; ++k) {
     for (int j=1; j<=ngh; ++j) {
       for (int i=is; i<=ie; ++i) {
         prim(IVX,k,js-j,i) = 0;
         prim(IVY,k,js-j,i) = 0;
         prim(IVZ,k,js-j,i) = 0;
-        prim(IDN,k,js-j,i) = pow(prim(IPR,k,js,i), 3./5.);
+        prim(IDN,k,js-j,i) = pow(prim(IPR,k,js-j,i), 3./5.);
       }
     }
   }
@@ -661,7 +661,7 @@ void LintInnerX2(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim,
     for (int k=ks; k<=ke+1; ++k) {
       for (int j=1; j<=ngh; ++j) {
         for (int i=is; i<=ie; ++i) {
-          b.x3f(k,(js-j),i) = b.x3f(k,js,i);
+          b.x3f(k,(js-j),i) = b.x3f(k,js+j-1,i);
         }
       } 
     }
