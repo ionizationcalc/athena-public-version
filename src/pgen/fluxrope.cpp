18d25f23e6a5b849308c5290ab4c0437e517a192
Chengcai Shen <chengcaishen@cfa.harvard.edu>
2018-11-21 01:31:45 -0500

    Update fluxrope.cpp
    Bug fixed in linetied BC.

--- src/pgen/fluxrope.cpp
+++ src/pgen/fluxrope.cpp
@@ -627,7 +627,6 @@ void LintInnerX2(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim,
         prim(IVX,k,js-j,i) = 0;
         prim(IVY,k,js-j,i) = 0;
         prim(IVZ,k,js-j,i) = 0;
-        prim(IND,k,js-j,i) = 10.0;
       }
     }
   }
@@ -657,7 +656,7 @@ void LintInnerX2(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim,
     for (int k=ks; k<=ke+1; ++k) {
       for (int j=1; j<=ngh; ++j) {
         for (int i=is; i<=ie; ++i) {
-          b.x3f(k,(js-j),i) = b.x3f(k,js+j-1,i);
+          b.x3f(k,(js-j),i) = b.x3f(k,js,i);
         }
       } 
     }
